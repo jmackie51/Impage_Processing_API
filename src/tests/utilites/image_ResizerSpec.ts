8bd0480fb0df37e5f192a769c349e707706a268e
jmackie51 <mackie51@hotmail.com>
2022-04-28 04:31:43 +0000

    "Revision 5"

--- src/tests/utilites/image_ResizerSpec.ts
+++ src/tests/utilites/image_ResizerSpec.ts
@@ -3,11 +3,15 @@ import express from 'express';
 import supertest from 'supertest';
 import app from '../../index';
 import resizingUtility from '../../utilities/Image_Resizer';
+import ResolvePath from 'path';
 
 const request = supertest(app);
 
 //Test that we do not get any errors when we request an image to be resized via the endpoint
 describe('2. Test the Image Resizing functionality', () => {
+    
+  const bPath = ResolvePath.resolve(__dirname, '../../../images');
+    
   it('Expect the resizing endpoint not to throw an error', async () => {
     const response = await request.get(
       '/api/resize/?filename=fjord&width=300&height=207'
@@ -17,11 +21,11 @@ describe('2. Test the Image Resizing functionality', () => {
   });
 
   //Test that we do NOT get any errors when we call the resizing utility
-  it('expects resizingUtility(`/home/workspace/images/full/fjord.jpg`, `/home/workspace/images/thumbnails/fjord_300_300.jpg`, 300,300) to be resolved', async () => {
+  it('expects resizingUtility(`${bPath}/full/fjord.jpg`, `${bPath}/thumbnails/fjord_300_300.jpg`, 300,300) to be resolved', async () => {
     await expectAsync(
       resizingUtility(
-        `/home/workspace/images/full/fjord.jpg`,
-        `/home/workspace/images/thumbnails/fjord_300_300.jpg`,
+        `${bPath}/full/fjord.jpg`,
+        `${bPath}/thumbnails/fjord_300_300.jpg`,
         300,
         300
       )
@@ -29,11 +33,11 @@ describe('2. Test the Image Resizing functionality', () => {
   });
 
   //Test that we DO get errors when we call the resizing utility with the name of an image that doesnt exist
-  it('expects resizingUtility(`/home/workspace/images/full/frd.jpg`, `/home/workspace/images/thumbnails/fjd_300_300.jpg`, 300,300) to be rejected', async () => {
+  it('expects resizingUtility(`${bPath}/full/frd.jpg`, `${bPath}/thumbnails/fjd_300_300.jpg`, 300,300) to be rejected', async () => {
     await expectAsync(
       resizingUtility(
-        `/home/workspace/images/full/frd.jpg`,
-        `/home/workspace/images/thumbnails/fjd_300_300.jpg`,
+        `${bPath}/full/frd.jpg`,
+        `${bPath}/thumbnails/fjd_300_300.jpg`,
         300,
         300
       )
